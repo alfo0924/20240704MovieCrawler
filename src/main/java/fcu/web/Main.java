8e7f5fff51982dd896ebc68622d8b8a78cdf4803
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-07-04 09:38:19 +0800

    v1.01 commit n push

--- src/main/java/fcu/web/Main.java
+++ src/main/java/fcu/web/Main.java
@@ -1,7 +1,13 @@
 package fcu.web;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        WebDriver driver = new ChromeDriver();
+
+
     }
 }
